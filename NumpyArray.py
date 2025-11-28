2539a05130c9f347050ebbfb87c51a2ee4bb80e6
Anuj Kumar <anujpandey61687@gmail.com>
2025-11-29 02:23:49 +0530

    first commit

---
+++ NumpyArray.py
@@ -0,0 +1,12 @@
+# Basic Array Creation Using Numpy
+import numpy as np
+
+#This is One-D and vector Array
+list = [12, 34, 67, 98, 43 ,23]
+arr = np.array(list)
+print(arr)
+
+# This is Two-D Array Creation 
+list = [[1, 2, 4, 5], [3, 4, 6, 8], [9, 7, 3, 2], [23, 4, 67, 8]]
+arr = np.array(list)
+print(arr)
---
+++ NumpyArray3.py
@@ -0,0 +1,28 @@
+#Array Attribute's and methods 
+
+
+import numpy as np
+
+# list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# arr = np.array(list)
+# print(arr.shape)
+# print(arr.size)
+# print(arr.dtype)
+
+# print("\n")
+
+
+# print(arr.min())
+# print(arr.max())
+# print(arr.mean())
+# sum1 = np.sum(arr, axis = 0)
+# sum2 = np.sum(arr, axis = 1)
+# print(sum1) # Column wise
+# print(sum2) # Row wise
+
+
+arr  = np.arange(1, 31)
+print(arr)
+
+
+
