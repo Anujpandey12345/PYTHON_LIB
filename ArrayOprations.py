1b1bbfa4d1c41005cc705d0b4cb40835e77f78e8
Anuj Kumar <anujpandey61687@gmail.com>
2025-11-29 12:49:25 +0530

    Push Advanvce

---
+++ ArrayOprations.py
@@ -0,0 +1,56 @@
+# In this We are learning Array Operations 
+import numpy as np
+
+# # This is Correct /\
+# a1 = np.array([1, 2, 3, 4, 5])
+# a2 = np.array([6, 7, 8, 9, 10])
+# print(a1+a2)
+# print(a1-a2)
+# print(a1*a2)
+# print(a1**a2)
+# print(a1/a2)
+# print(a1//a2)
+
+
+# This Gives an Error 
+# a1 = np.array([1, 2, 3, 4, 5])
+# a2 = np.array([6, 7, 8, 9, 10, 11])
+# print(a1+a2)
+
+
+#BroadCasting
+# arr = np.array([10, 20, 30, 40, 50])
+# array_broad = arr + 10
+# print(array_broad)
+# array_broad = arr - 10
+# print(array_broad)
+# array_broad = arr * 10
+# print(array_broad)
+# array_broad = arr / 10
+# print(array_broad)
+# array_broad = arr // 10
+# print(array_broad)
+# array_broad = arr ** 10
+# print(array_broad)
+
+
+
+# arr = np.arange(1, 21)
+# # slice = arr[:5]
+# # slice = slice * 5
+# # print(slice)
+# # print(arr)
+
+# b = arr.copy()
+# b[0] = 1000
+# print(arr)
+
+
+# A = np.array([[1, 2], [3, 4]])
+# B = np.array([[5, 6], [7, 8]])
+# print(A * B)
+# print(A @ B)
+
+
+
+#Advance 
